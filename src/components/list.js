67be1b3a76b9d8984e011106e8c5a61e5e9bdfc6
Scott Bowler <scott.bowler@learningfuze.com>
2018-12-19 18:05:10 -0800

    Add item in progress

--- src/components/list.js
+++ src/components/list.js
@@ -1,35 +1,16 @@
 import React, { Component } from 'react';
-import dummyList from '../data/to_do_list';
 
-class List extends Component {
-    state = {
-        list: []
-    }
-
-    componentDidMount(){
-        this.getListData();
-    }
-
-    getListData(){
-        // Call server to get list data
-
-        this.setState({
-            list: dummyList
-        });
-    }
-
-    render(){
+const List = (props) => {
         
-        const listElements = this.state.list.map((item) => {
-            return <li key={item._id} className="collection-item">{item.title}</li>
-        });
+    const listElements = props.toDos.map((item) => {
+        return <li key={item._id} className="collection-item">{item.title}</li>
+    });
 
-        return (
-            <ul className="collection">
-                {listElements}
-            </ul>
-        );
-    }
+    return (
+        <ul className="collection">
+            {listElements}
+        </ul>
+    );
 }
 
 export default List;
